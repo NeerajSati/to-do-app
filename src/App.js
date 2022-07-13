d740dd010a0da0d1f6e9151cd6156c2c06f0ec01
Neeraj Sati <neeraj.sati123@gmail.com>
2022-07-13 13:23:10 +0530

    Title Added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ToDoApp from './components/ToDoApp';
 function App() {
   return (
     <div className="App">
+      <div className='appDetails'>To Do App</div>
       <ToDoApp/>
     </div>
   );
